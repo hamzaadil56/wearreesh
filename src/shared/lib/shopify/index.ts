b9bc2203609d459f10a07313a1a86feca7d50990
Muhammad Hamza <hamzaadil56@gmail.com>
2025-10-12 23:56:23 +0500

    removed force-cache and fixed filtering logic

--- src/shared/lib/shopify/index.ts
+++ src/shared/lib/shopify/index.ts
@@ -268,7 +268,6 @@ export async function getCart(cartId: string): Promise<Cart | undefined> {
 		return undefined;
 	}
 	const res = await shopifyFetch<ShopifyCartOperation>({
-		cache: "force-cache",
 		query: getCartQuery,
 		variables: { cartId },
 		tags: [TAGS.cart],
@@ -435,7 +434,6 @@ export async function getProducts({
 			reverse,
 			sortKey,
 		},
-		cache: "force-cache",
 	});
 
 	return reshapeProducts(removeEdgesAndNodes(res.body.data.products));
